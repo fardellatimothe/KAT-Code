3e048ff46c788c39f67715d4d368093213a51952
fardellatimothe <timothe.fardella@viacesi.fr>
2025-02-14 16:19:11 +0100

    Update page accueil

---
+++ scripts/home-script.js
@@ -0,0 +1,19 @@
+function startCourse(button) {
+    const card = button.closest('.course-card');
+    const progress = card.querySelector('.progress');
+
+    let currentProgress = 0;
+    button.disabled = true;
+    button.style.opacity = "0.7";
+
+    const interval = setInterval(() => {
+        currentProgress += 1;
+        progress.style.width = `${currentProgress}%`;
+
+        if (currentProgress >= 100) {
+            clearInterval(interval);
+            button.disabled = false;
+            button.style.opacity = "1";
+        }
+    }, 50);
+}
