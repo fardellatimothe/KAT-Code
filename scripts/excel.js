c75072b4c3803f680739785ee994e7ead40a50be
fardellatimothe <timothe.fardella@viacesi.fr>
2025-03-01 00:46:20 +0100

    Create Excel Course

---
+++ scripts/excel.js
@@ -0,0 +1,26 @@
+// Changement des chapitres
+function changeChapter(chapterId) {
+    localStorage.setItem("currentChapter", chapterId);
+
+    document.querySelectorAll(".section").forEach(section => {
+        section.classList.remove("active");
+    });
+
+    document.getElementById(chapterId).classList.add("active");
+
+    document.querySelectorAll('.nav-item').forEach(nav => nav.classList.remove('active'));
+    let navItem = document.querySelector(`.nav-item[onclick="changeChapter('${chapterId}')"]`);
+    if (navItem) {
+        navItem.classList.add('active');
+    }
+
+    window.scrollTo({ top: 0 });
+}
+
+// Actualisation de la page
+document.addEventListener("DOMContentLoaded", function() {
+    let savedChapter = localStorage.getItem("currentChapter");
+    if (savedChapter) {
+        changeChapter(savedChapter);
+    }
+});
